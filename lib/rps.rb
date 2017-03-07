3306f42c48d8fd124ccf4a050b8b06941df45c53
alicespyglass <alicespyglass@gmail.com>
2017-03-07 15:58:23 +0000

    added paper

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,5 +2,7 @@ def rock_paper_scissors(input)
  # If input is equal to 1, return rock
 if input.downcase == "rock"
   'Rock'
+else input.downcase == "paper"
+  'Paper'
  end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,7 +1,10 @@
 require '../lib/rps.rb'
 
 describe 'rock paper scissors game' do
-  it 'returns "rock" when input is Rock' do # Given input Rock
+  it 'returns "Rock" when input is Rock' do # Given input Rock
     expect(rock_paper_scissors("Rock")).to eq 'Rock' # Returns Rock when RPS is run
+  end
+  it 'returns "Paper" when input is paper' do
+    expect(rock_paper_scissors("Paper")).to eq 'Paper'
   end
 end

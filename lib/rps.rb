8ff602d185c7fd0a2092480e1f058e4d5bb882e6
Magnus Holm <mail@magnusholm.com>
2017-03-07 18:47:07 +0000

    created compare_choices method and passed tests for draw outcome

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,4 +1,4 @@
-def rock_paper_scissors(input)
+def user_input(input)
   user_choice = ""
  # If input is equal to 1, return rock
 if input.downcase == "rock"
@@ -8,18 +8,32 @@ elsif input.downcase == "paper"
 else (input.downcase == "scissors") || (input.downcase == "scissor")
   user_choice = 'Scissors'
  end
- "#{user_choice}, #{computer_choice}"
- end
+ user_choice
+end
 
 
-def computer_choice
+def computer_input
   random_num =  rand(1..3)
   # if x = 1, return "Rock"
+  computer_choice = ""
   if random_num == 1
-    "Rock"
+    computer_choice = "Rock"
   elsif random_num == 2
-    "Paper"
+    computer_choice = "Paper"
   elsif random_num == 3
-    "Scissors"
+    computer_choice = "Scissors"
   end
+  computer_choice
+end
+
+def compare_choices(x)
+  #When user choice is "rock" and computer choice is "rock" return draw.
+  if user_input(x) == computer_input
+    'Draw'
+  end
+
+#When user choice is "rock" and computer choice is "scissors" return win.
+#When user choice is "rock" and computer choice is "paper" return lose.
+
+
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -2,6 +2,12 @@ require '../lib/rps.rb'
 
 describe 'Rock Paper Scissors game' do
   it 'returns user input and random computer choice' do
-    expect(rock_paper_scissors("Rock")).to eq("Rock, Rock").or eq("Rock, Paper").or eq("Rock, Scissors")
+    expect(user_input("Rock")).to eq("Rock, Rock").or eq("Rock, Paper").or eq("Rock, Scissors")
+  end
+  it 'returns true when user input is the same as computer choice' do
+    expect(compare_choices("rock")).to eq true
+  end
+  it 'returns "draw" when user input is the same as computer choice' do
+    expect(compare_choices("rock")).to eq "Draw"
   end
 end
